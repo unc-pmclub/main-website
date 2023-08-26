dd5c0d26b1e86eae1c5632bd41dd5a0574e1f850
Noah Davis <noahdavis35@att.net>
2023-08-26 19:34:28 -0400

    Added mock data to PM Portal

--- src/pages/PMPortal.jsx
+++ src/pages/PMPortal.jsx
@@ -3,9 +3,21 @@ import PortalHeader from "../partials/PortalHeader";
 import ListContainer from "../partials/PortalListContainer";
 
 function PMPortal() {
-  // This is a placeholder for the user's name, replace with actual name once you implement auth flow
+  // This is a placeholder for the user's name, to be replaced with user'sn name
   const userName = "John Doe";
-  const quickLinksSections = [
+
+  const aboutSection = [
+    {
+      listItems: [
+        {
+          name: "This is a page for members to quickly access PM and PM Club related materials.",
+        },
+      ],
+    },
+  ];
+
+  // Structure for containers with multiple sections, no item description
+  const quickLinksSection = [
     {
       title: "General",
       listItems: [
@@ -27,8 +39,129 @@ function PMPortal() {
           link: "https://www.atlassian.com/software/jira",
           internal: false,
         },
-        { name: "Miro", link: "https://www.miro.com/", internal: true },
-        { name: "Figma", link: "https://www.figma.com", internal: true },
+        { name: "Miro", link: "https://www.miro.com/", internal: false },
+        { name: "Figma", link: "https://www.figma.com", internal: false },
+      ],
+    },
+  ];
+
+  // Structure for containers with one section with item descriptions
+  const jobPostingsSection = [
+    {
+      listItems: [
+        {
+          name: "Job Posting 1",
+          link: "#",
+          internal: false,
+          description: "Product Management",
+        },
+        {
+          name: "Job Posting 2",
+          link: "#",
+          internal: false,
+          description: "Software Engineering",
+        },
+        {
+          name: "Job Posting 3",
+          link: "#",
+          internal: false,
+          description: "Product Management",
+        },
+      ],
+    },
+  ];
+
+  const pmLabSection = [
+    {
+      listItems: [
+        {
+          name: "Working with Figma",
+          link: "#",
+          internal: false,
+          description: "A brief video on the basic functionality of Figma",
+        },
+        {
+          name: "Being a successful Project Manager",
+          link: "#",
+          internal: false,
+          description:
+            "A video overviewing how to be a good Project Manager in Lab",
+        },
+      ],
+    },
+  ];
+
+  const pmRecruitingSection = [
+    {
+      listItems: [
+        {
+          name: "Atlassian APM Guide",
+          link: "#",
+          internal: false,
+          description: "I will think of a description later",
+        },
+        {
+          name: "Google APM Interview Guide",
+          link: "#",
+          internal: false,
+          description: "I will think of this description later-er",
+        },
+        {
+          name: "MongoDB 2023 Summer Intern Guide",
+          link: "#",
+          internal: false,
+          description: "I will think of this description later-er-er",
+        },
+      ],
+    },
+  ];
+
+  // Structure for containers with multiple sections and descriptions
+  const additionalLearningSection = [
+    {
+      title: "Podcasts",
+      listItems: [
+        {
+          name: "Spotify: A Product Story",
+          link: "#",
+          internal: false,
+          description: "blah blah blah",
+        },
+        {
+          name: "The Product Podcast",
+          link: "#",
+          internal: false,
+          description: "blah blah blah",
+        },
+        {
+          name: "Product Talk",
+          link: "#",
+          internal: false,
+          description: "blah blah blah",
+        },
+        {
+          name: "NN/g UX Podcast",
+          link: "#",
+          internal: false,
+          description: "blah blah blah",
+        },
+      ],
+    },
+    {
+      title: "Articles & Guides",
+      listItems: [
+        {
+          name: "Asana",
+          link: "#",
+          internal: false,
+          description: "blah blah blah",
+        },
+        {
+          name: "Atlassian",
+          link: "#",
+          internal: false,
+          description: "blah blah blah",
+        },
       ],
     },
   ];
@@ -58,77 +191,35 @@ function PMPortal() {
         </h2>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-5 md:grid-rows-6 gap-8 max-w-6xl mx-auto mt-12 overflow-auto">
+        {/* Quick Links */}
         <div className="md:row-span-3">
-          <ListContainer title="Quick Links" sections={quickLinksSections} />
+          <ListContainer title="Quick Links" sections={quickLinksSection} />
         </div>
+        {/* Job Postings */}
         <div className="md:row-span-3 md:col-start-1 md:row-start-4">
-          <div className="bg-white rounded shadow-lg p-4">
-            <div className="bg-red-500 text-white font-bold p-2 rounded-t-lg">
-              Job Postings
-            </div>
-            <div className="text-black p-2">
-              <ul>
-                <li>Item 1</li>
-                <li>Item 2</li>
-                <li>Item 3</li>
-              </ul>
-            </div>
-          </div>
+          <ListContainer title="Job Postings" sections={jobPostingsSection} />
         </div>
+        {/* About */}
         <div className="md:row-span-2 md:col-start-5 md:row-start-1">
-          <div className="bg-white rounded shadow-lg p-4">
-            <div className="bg-red-500 text-white font-bold p-2 rounded-t-lg">
-              About this Page
-            </div>
-            <div className="text-black p-2">
-              <p>
-                This is a page for members to quickly access PM (Club) related
-                materials.
-              </p>
-            </div>
-          </div>
+          <ListContainer title="About this Page" sections={aboutSection} />
         </div>
+        {/* Additional Learning */}
         <div className="md:row-span-4 md:col-start-5 md:row-start-3">
-          <div className="bg-white rounded shadow-lg p-4">
-            <div className="bg-red-500 text-white font-bold p-2 rounded-t-lg">
-              Additional Learning
-            </div>
-            <div className="text-black p-2">
-              <ul>
-                <li>Item 1</li>
-                <li>Item 2</li>
-                <li>Item 3</li>
-              </ul>
-            </div>
-          </div>
+          <ListContainer
+            title="Additional Learning"
+            sections={additionalLearningSection}
+          />
         </div>
+        {/* PM Lab Resources */}
         <div className="md:col-span-3 md:row-span-2 md:col-start-2 md:row-start-1">
-          <div className="bg-white rounded shadow-lg p-4">
-            <div className="bg-red-500 text-white font-bold p-2 rounded-t-lg">
-              PM Lab Resources
-            </div>
-            <div className="text-black p-2">
-              <ul>
-                <li>Item 1</li>
-                <li>Item 2</li>
-                <li>Item 3</li>
-              </ul>
-            </div>
-          </div>
+          <ListContainer title="PM Lab Resources" sections={pmLabSection} />
         </div>
+        {/* PM Recruiting & Interviewing */}
         <div className="md:col-span-3 md:row-span-3 md:col-start-2 md:row-start-3">
-          <div className="bg-white rounded shadow-lg p-4">
-            <div className="bg-red-500 text-white font-bold p-2 rounded-t-lg">
-              PM Recruiting & Interviewing Resources
-            </div>
-            <div className="text-black p-2">
-              <ul>
-                <li>Item 1</li>
-                <li>Item 2</li>
-                <li>Item 3</li>
-              </ul>
-            </div>
-          </div>
+          <ListContainer
+            title="PM Recruiting & Interviewing Resources"
+            sections={pmRecruitingSection}
+          />
         </div>
       </div>
     </div>
--- src/partials/PortalListContainer.jsx
+++ src/partials/PortalListContainer.jsx
@@ -1,13 +1,14 @@
+// TODO:
+// - Modify implementation to take in "type" from section data and display list or paragraph
+
 import React from "react";
 
 function ListContainer({ title, sections }) {
   return (
     <div className="bg-white rounded shadow-lg relative">
-      {/* Make this div position absolute and full width */}
       <div className="bg-red-500 text-white font-bold p-2 rounded-t-lg absolute top-0 left-0 w-full">
         {title}
       </div>
-      {/* Add padding at the top to ensure the content starts below the header */}
       <div className="text-black p-2 pt-10">
         {sections.map((section, sectionIndex) => (
           <div key={sectionIndex}>
