a3270c2496c7a51249ff6fc1f12a9c78c5dfea90
Noah Davis <noahdavis35@att.net>
2023-11-30 17:14:17 -0500

    fixing reset password

--- src/auth/reset-password.js
+++ src/auth/reset-password.js
@@ -2,7 +2,7 @@ import {useState} from "react";
 import { auth } from "../../firebase.config";
 import { sendPasswordResetEmail } from "firebase/auth";
 
-export default function sendPasswordReset () {
+export default function sendPasswordReset (email) {
     sendPasswordResetEmail(auth, email)
     .then(() => {
         // Password reset email successfully sent
