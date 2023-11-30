e3fb60267af5d5216421a714c0c89c9a3ffb0b8f
Noah Davis <noahdavis35@att.net>
2023-11-30 07:53:04 -0500

    gh pages deploy

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/uncpm-dev-website/",
+  base: "/",
   define: {
     'process.env': process.env
   },
