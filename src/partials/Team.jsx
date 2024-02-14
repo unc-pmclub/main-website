66dabb4c5a5fb6f7fb9dc68b2332cf68eded9fd6
Jennifer Lee <jclee1@unc.edu>
2024-02-14 16:06:52 -0500

    headshots added

--- src/partials/Team.jsx
+++ src/partials/Team.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
-import Team1 from '../images/Headshots/17.jpg';
-import Team2 from '../images/Headshots/16.jpg';
+import Team1 from '../images/Headshots/22.png';
+import Team2 from '../images/Headshots/21.jpg';
 import Team3 from '../images/Headshots/18.jpg';
 import Team4 from '../images/Headshots/19.jpg';
-import Team5 from '../images/Headshots/profilephto.jpeg';
+import Team5 from '../images/Headshots/23.jpeg';
+import Team6 from '../images/Headshots/24.jpg';
+import Team7 from '../images/Headshots/25.jpg';
 
 
 function Team() {
@@ -67,12 +69,60 @@ function Team() {
                 </div>
                 <div class="w-full bg-white rounded-lg p-12 flex flex-col justify-center items-center">
                     <div class="mb-8">
+                        <img class="object-center object-cover rounded-full h-36 w-36" src={Team2} alt="photo"/>
+                    </div>
+                    <div class="text-center">
+                        <p class="text-xl text-gray-700 font-bold mb-2">Daniel Lee</p>
+                        <p class="text-base text-gray-400 font-normal mb-2">VP of Marketing</p>
+                        <a href="https://www.linkedin.com/in/ddlee1/">
+                            <i class="fa-brands fa-linkedin fa-2x text-gray-900 hover:text-red-500 duration-500"></i>
+                        </a>
+                    </div>
+                </div>
+                <div class="w-full bg-white rounded-lg p-12 flex flex-col justify-center items-center">
+                    <div class="mb-8">
+                        <img class="object-center object-cover rounded-full h-36 w-36" src={Team1} alt="photo"/>
+                    </div>
+                    <div class="text-center">
+                        <p class="text-xl text-gray-700 font-bold mb-2">Tracy Gonsalves</p>
+                        <p class="text-base text-gray-400 font-normal mb-2">VP of External Affairs</p>
+                        <a href="https://www.linkedin.com/in/tracygonsalves">
+                            <i class="fa-brands fa-linkedin fa-2x text-gray-900 hover:text-red-500 duration-500"></i>
+                        </a>
+                    </div>
+                </div>
+                <div class="w-full bg-white rounded-lg p-12 flex flex-col justify-center items-center">
+                    <div class="mb-8">
+                        <img class="object-center object-cover rounded-full h-36 w-36" src={Team6} alt="photo"/>
+                    </div>
+                    <div class="text-center">
+                        <p class="text-xl text-gray-700 font-bold mb-2">Tejas Chandra</p>
+                        <p class="text-base text-gray-400 font-normal mb-2">Director of Labs</p>
+                        <a href="https://www.linkedin.com/in/tejaschandra/">
+                            <i class="fa-brands fa-linkedin fa-2x text-gray-900 hover:text-red-500 duration-500"></i>
+                        </a>
+                    </div>
+                </div>
+                <div class="w-full bg-white rounded-lg p-12 flex flex-col justify-center items-center">
+                    <div class="mb-8">
+                        <img class="object-center object-cover rounded-full h-36 w-36" src={Team7} alt="photo"/>
+                    </div>
+                    <div class="text-center">
+                        <p class="text-xl text-gray-700 font-bold mb-2">Arya Salian</p>
+                        <p class="text-base text-gray-400 font-normal mb-2">Technical Leader</p>
+                        <a href="https://www.linkedin.com/in/arya-salian-462a9421b/">
+                            <i class="fa-brands fa-linkedin fa-2x text-gray-900 hover:text-red-500 duration-500"></i>
+                        </a>
+                    </div>
+                </div>
+                <div class="w-full bg-white rounded-lg p-12 flex flex-col justify-center items-center">
+                    <div class="mb-8">
                         <img class="object-center object-cover rounded-full h-36 w-36" src={Team5} alt="photo"/>
                     </div>
                     <div class="text-center">
                         <p class="text-xl text-gray-700 font-bold mb-2">Jennifer Lee</p>
-                        <p class="text-base text-gray-400 font-normal mb-2">Website Manager</p>
-                        <a href="https://www.linkedin.com/in/ryan-s-richardson/">
+                        <p class="text-base text-gray-400 font-normal mb-2">Web Developer</p>
+                        <a href="https://www.linkedin.com/in/jennifer-lee-817316292/">
                             <i class="fa-brands fa-linkedin fa-2x text-gray-900 hover:text-red-500 duration-500"></i>
                         </a>
                     </div>
